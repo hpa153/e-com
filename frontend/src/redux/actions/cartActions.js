d6ed8dddefe3bb3644722bd68e2448ccc844a96b
Phuong Anh Hoang <phuonganhhoang.153@gmail.com>
2023-10-23 20:19:31 -0400

    Add cart functionality to frontend

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,7 +1,21 @@
 import * as actionTypes from "../constants/cartConstants";
+import axios from "axios";
 
-export const addToCart = () => (dispatch) => {
-  dispatch({
-    type: actionTypes.ADD_TO_CART,
-  });
-};
+export const addToCart =
+  (productId, quantity) => async (dispatch, getState) => {
+    const product = await axios.get("/api/products/product/" + productId);
+
+    dispatch({
+      type: actionTypes.ADD_TO_CART,
+      payload: {
+        productId,
+        name: product.data.name,
+        price: product.data.price,
+        image: product.data.images[0] ?? null,
+        count: product.data.count,
+        quantity: +quantity,
+      },
+    });
+
+    localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+  };
--- frontend/src/redux/reducers/cartReducer.js
+++ frontend/src/redux/reducers/cartReducer.js
@@ -1,9 +1,46 @@
 import * as actionTypes from "../constants/cartConstants";
 
-const cartReducer = (state = { value: 0 }, action) => {
+export const INITIAL_CART = {
+  cartItems: [],
+  itemsCount: 0,
+  cartSubtotal: 0,
+};
+
+const cartReducer = (state = INITIAL_CART, action) => {
   switch (action.type) {
     case actionTypes.ADD_TO_CART: {
-      return state;
+      const toBeAddedProduct = action.payload;
+      const existingProduct = state.cartItems.find(
+        (prod) => prod.productId === toBeAddedProduct.productId
+      );
+
+      const newCart = { ...state };
+      if (existingProduct) {
+        newCart.itemsCount = 0;
+        newCart.cartSubtotal = 0;
+
+        newCart.cartItems = state.cartItems.map((prod) => {
+          if (prod.productId === toBeAddedProduct.productId) {
+            newCart.itemsCount += toBeAddedProduct.quantity;
+            newCart.cartSubtotal +=
+              toBeAddedProduct.price * toBeAddedProduct.quantity;
+          } else {
+            newCart.itemsCount += prod.quantity;
+            newCart.cartSubtotal += prod.price * prod.quantity;
+          }
+
+          return prod.productId === toBeAddedProduct.productId
+            ? toBeAddedProduct
+            : prod;
+        });
+      } else {
+        newCart.itemsCount += toBeAddedProduct.quantity;
+        newCart.cartSubtotal +=
+          toBeAddedProduct.price * toBeAddedProduct.quantity;
+        newCart.cartItems.push(toBeAddedProduct);
+      }
+
+      return newCart;
     }
     default: {
       return state;
